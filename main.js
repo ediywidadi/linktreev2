6eb27d7008b76744a763f8194940ededdccebc0b
widadediy@gmail.com <widadediy@gmail.com>
2023-02-23 21:26:33 +0700

    Perubahan Link

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function share() {
     navigator.share({
       title: 'Linktree',
       text: 'Linktree By Widadi',
-      url: 'https://www.contoh.com'
+      url: 'https://ediywidadi.github.io/linktreev2/'
     })
     .then(() => console.log('Berhasil membagikan link'))
     .catch(error => console.error('Error sharing:', error));
